4cddd57c0ee09f732358f91f40e8db2ccf12b7f8
nelson <nelson_zarate@eticalgarve.com>
2026-03-02 21:01:16 +0000

    add: framer motion

--- Aula6/projeto-next-aula/src/components/layout/HomePage.tsx
+++ Aula6/projeto-next-aula/src/components/layout/HomePage.tsx
@@ -6,14 +6,18 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import CounterRedux from "@/redux/counter";
 import Counter from "../ui/Counter";
+import {motion} from "framer-motion"
 const HomePage = () => {
 	const { tema } = useContext(TemaContext);
 	return (
-		<div className={`flex min-h-screen items-center justify-center font-sans ${tema === "claro" ? "bg-zinc-50" : "bg-black"} `}>
+		<motion.div 
+		
+		// initial={{opacity: 0 , x: -100 , y: -100}} animate={{ opacity: 1, x: 0,y: 0}} transition={{duration:1}}
+		className={`flex min-h-screen items-center justify-center font-sans ${tema === "claro" ? "bg-zinc-50" : "bg-black"} `}>
 			<h1> Home </h1>
 			<CounterRedux />
       		<BotaoTema />
-		</div>
+		</motion.div>
 	)
 }
 
--- Aula6/projeto-next-aula/src/redux/counter.tsx
+++ Aula6/projeto-next-aula/src/redux/counter.tsx
@@ -3,18 +3,29 @@
 import { useSelector,useDispatch } from "react-redux";
 import {RootState} from "./store";
 import { increment, decrement, incrementByAmount,reset } from "./counterSlice";
-
+import {motion} from "framer-motion"
 const CounterRedux = () => {
 	const counter = useSelector((state: RootState) => state.counter.value);
 	const dispatch = useDispatch();
 	return (
-		<div className="flex flex-col items-center gap-4 bg-black text-white p-4 rounded">
+		<motion.div 
+		           animate={{
+                scale: [1, 2, 2, 1, 1],
+                rotate: [0, 0, 180, 180, 0],
+                borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+            }}
+            transition={{
+                duration: 2,
+                ease: "easeInOut",
+                times: [0, 0.2, 0.5, 0.8, 1]
+            }}
+		className="flex flex-col items-center gap-4 bg-black text-white p-4 rounded">
 			<p>Contador: {counter}</p>
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(increment())}>Incrementar</button>
 			<button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(decrement())}>Decrementar</button>
 			<button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(incrementByAmount(5))}>Incrementar por 5</button>	
 			<button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(reset())}> Reset</button>
-		</div>
+		</motion.div>
 	)
 }
 
